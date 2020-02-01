4526751ce2799abe4adfca0efe8cf254753c86ca
042mgs.github.io <ad.snoopso@gmail.com>
2020-02-01 17:49:56 +0900

    4week

---
+++ myproject/4week/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/topsong', methods=['GET'])
+def test_get():
+   title_receive = request.args.get('title_give')
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
